3315690ed650470b9f4c1da56e66f37d7744582e
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2024-08-18 21:04:05 +0530

    added UI components

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
 					</SignedIn>
 					<SignedOut>
 						<Button asChild className="rounded-full" size={"lg"}>
-							<Link href={"/signin"}>Login</Link>
+							<Link href={"/sign-in"}>Login</Link>
 						</Button>
 					</SignedOut>
 				</div>
